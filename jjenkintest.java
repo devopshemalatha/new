fc4f8f5acaa7a0edc713e8a0e74290cda3cf8608
someone <someone@someplace.com>
2022-04-28 18:08:38 +0530

    second submission

--- jjenkintest.java
+++ jjenkintest.java
@@ -1,12 +1,14 @@
 
 public class jjenkintest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
-for(int i=0;i<7;i++)
-{
-	System.out.println(i);
-	}
+		for(int i=0;i<7;i++)
+		{
+			System.out.println(i);
+			System.out.println(i+1);
+		}
 	}
 
 }
